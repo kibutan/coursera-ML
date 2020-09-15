a14926e9afce7a5f98e07c175a76b0fb3c79d9b9
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-09-15 15:45:57 +0900

    Update costFunction.m

--- ex02/costFunction.m
+++ ex02/costFunction.m
@@ -21,9 +21,8 @@ grad = zeros(size(theta));
 %
 h = sigmoid(X*theta);
 J = 1/m*(-y'*log(h)-(1-y)'*log(1-h));
-
-theta = theta - alpha/m*X'*(sigmoid(X*theta)-y)
-
+theta = theta - 1/m*X'*(h-y);
+grad = 1/m*(h-y)'*X;
 
 
 
